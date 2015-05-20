fce423d00dfbc19bfa37c7a6bab4bd43cfe2efc7
Peng Guanwen <pgw1999@foxmail.com>
2015-05-20 16:26:06 +0800

    新建单元测试

---
+++ AuthSharp.Tests/Controllers/UserControllerTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AuthSharp.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace AuthSharp.Tests
+{
+    [TestClass()]
+    public class UserControllerTests
+    {
+        UserController userController = new UserController();
+        [TestMethod()]
+        public void LoginTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void PortalTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void MessageTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
